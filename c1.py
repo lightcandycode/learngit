7f79a3739aea8afe8a25eed1eb986c77f921f21b
lightcandycode <1819532219@qq.com>
2019-05-07 19:28:17 +0800

    c1corre

--- c1.py
+++ c1.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-
+import turtle
 content = requests.get('http://www.baidu.com').content
 soup = BeautifulSoup(content, 'html.parser')
 for div in soup.find_all('div', {'class': 'content'}):
     print(div.text.strip())
 print(5+10)
+print(7, 97)
+print(5**2)
+print(2**7)
